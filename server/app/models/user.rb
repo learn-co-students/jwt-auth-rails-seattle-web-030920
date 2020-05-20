4760bf3f540afa0a2ac67b98dcf126959ee3b858
Roger Robertson Jr <rjrjr@pm.me>
2020-05-20 09:43:35 -0700

    Done.

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
+  # attr_accessor :password
   has_secure_password
   validates :username, uniqueness: { case_sensitive: false }
+
+  # def authenticate(pwd)
+  #   if BCrypt::Password.new(self.password_digest) == pwd
+  #     self
+  #   else
+  #     false
+  #   end
+  # end
 end
